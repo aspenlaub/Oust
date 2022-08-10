d4e4d8e9b8105e26a0e6337521de0247e337f3fa
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2022-08-10 22:23:03 +0200

    Package/-s updated to most recent version

---
+++ src/Model/Migrations/20220810202148_EntityFrameworkCore608.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aspenlaub.Net.GitHub.CSharp.Oust.Model.Migrations {
+    public partial class EntityFrameworkCore608 : Migration {
+        protected override void Up(MigrationBuilder migrationBuilder) {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder) {
+
+        }
+    }
+}
