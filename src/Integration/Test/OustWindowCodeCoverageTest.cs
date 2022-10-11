cfc1cb0ebddaa060ddce80a246dc0d5b07ff932e
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2022-10-11 20:05:27 +0200

    Code coverage test failed, probably after ajax optimizations in the Outrap framework

--- src/Integration/Test/OustWindowCodeCoverageTest.cs
+++ src/Integration/Test/OustWindowCodeCoverageTest.cs
@@ -46,7 +46,7 @@ public class OustWindowCodeCoverageTest : OustIntegrationTestBase {
         await sut.RemotelyProcessTaskListAsync(process, tasks, false, (_, _) => Task.CompletedTask);
         tasks.Clear();
 
-        const int expectedNumberOfLogFiles = 8;
+        const int expectedNumberOfLogFiles = 6;
         var resultFile = (await folderResolver.ResolveAsync(@"$(WampRoot)\temp\coverage", errorsAndInfos)).FullName + "\\" + dumperNameConverter.ScriptFileFriendlyShortName("Code Coverage") + ".txt";
         Assert.IsFalse(errorsAndInfos.AnyErrors(), errorsAndInfos.ErrorsToString());
         Assert.IsTrue(File.Exists(resultFile));
