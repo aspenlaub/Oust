9b70dfaf29d6167c4d78309f335e2ff90815a703
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2023-02-16 12:46:53 +0100

    Package/-s updated to most recent version

---
+++ src/Model/Migrations/20230216114252_EntityFrameworkCore703.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aspenlaub.Net.GitHub.CSharp.Oust.Model.Migrations {
+    /// <inheritdoc />
+    public partial class EntityFrameworkCore703 : Migration {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) {
+
+        }
+    }
+}
