912f3e473abea3d4334da8aa8ec973517b56b02f
VerBot <verbot@aspenlaub.net>
2026-01-15 00:11:46 +0100

    Package/-s updated to most recent version

--- src/Model/Migrations/20260114221644_MicrosoftEntityFrameworkCoreTools1002.Designer.cs
+++ src/Model/Migrations/20260114221644_MicrosoftEntityFrameworkCoreTools1002.Designer.cs
@@ -3,6 +3,7 @@ using Aspenlaub.Net.GitHub.CSharp.Oust.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Aspenlaub.Net.GitHub.CSharp.Oust.Model.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20260114221644_MicrosoftEntityFrameworkCoreTools1002")]
+    partial class MicrosoftEntityFrameworkCoreTools1002
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
